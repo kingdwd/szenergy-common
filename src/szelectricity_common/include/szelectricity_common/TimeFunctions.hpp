9901fc79d28d0421246d186f6b022d313511178f
Hajdu Csaba <kyberszittya@protonmail.ch>
2018-09-12 23:33:19 +0200

    Added time synchronization

---
+++ src/szelectricity_common/include/szelectricity_common/TimeFunctions.hpp
@@ -0,0 +1,23 @@
+#ifndef SZENERGY_TIME_FUNCTIONS_HPP
+#define SZENERGY_TIME_FUNCTIONS_HPP
+
+#include <ros/ros.h>
+
+namespace szenergy
+{
+    bool SynchronizeRosTime()
+    {
+        // Wait for clock synchronization
+        auto t_last_update = ros::Time::now();
+        while(ros::ok())
+        {
+            t_last_update = ros::Time::now();            
+            if (t_last_update.toSec()!=0.0)
+            {
+                return true;
+            }
+        }
+    }
+}
+
+#endif
